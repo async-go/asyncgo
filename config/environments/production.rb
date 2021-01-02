29929aef56b9f1a83c06abb5445474fec7997fe0
Matija Čupić <matteeyah@gmail.com>
2021-01-02 23:51:54 +0100

    Configure ActionMailer for Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'asyngo.com' }
+
+  # SMTP settings for Google Mail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['GOOGLE_MAIL_USERNAME'],
+    password: ENV['GOOGLE_MAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
