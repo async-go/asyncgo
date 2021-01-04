9ac1cbe3bbf1ba97d7895192fc8c477804c89e4f
Jason Yavorska <jason@metaluna.io>
2021-01-04 13:16:23 +0100

    Update specs

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'Homepage', type: :system do
   it 'shows the homepage' do
     visit '/'
 
-    expect(page).to have_text('Hello, world!')
+    expect(page).to have_text('Welcome to AsyncGo')
   end
 end
