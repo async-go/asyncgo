b805e2c2bd8759fb1200e419294bfc538878ec88
Jason Yavorska <jason@metaluna.io>
2021-01-14 17:00:47 +0100

    Redirect to create team page if user doesn't have one

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    redirect_to new_team_path if current_user.present? && current_user.team_id.blank?
+  end
 end
