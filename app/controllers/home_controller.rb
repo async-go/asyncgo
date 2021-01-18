8f2886040c195a685f6f6643968b592e6c90bdae
Matija Čupić <matteeyah@gmail.com>
2021-01-18 10:10:22 +0100

    Add user guidance to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index
-    redirect_to new_team_path if current_user.present? && current_user.team_id.blank?
-  end
+  def index; end
 end
