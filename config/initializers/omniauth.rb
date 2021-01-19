54aafa1ad897cd2247fd93e83d50f403d1b5276e
Jason Yavorska <jason@metaluna.io>
2021-01-19 16:41:54 +0100

    Reformat provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            {
              prompt: 'select_account'
            }
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user:email'
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'],
+          {
+            scope: 'user:email'
+          }
 end
