139a5e0c031b8c0423532cf278d80e6aaa1d1891
Jason Yavorska <jason@metaluna.io>
2021-01-19 16:32:25 +0100

    Move auth to private function

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OmniauthCallbacksController < ApplicationController
-  def google_oauth2
+  def handle_auth
     user = User.from_omniauth(request.env['omniauth.auth'])
 
     if user.persisted?
@@ -12,18 +12,12 @@ class OmniauthCallbacksController < ApplicationController
     end
 
     redirect_to root_path
+  end
+
+  def google_oauth2
+    handle_auth
   end
 
   def github
-    user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if user.persisted?
-      flash[:success] = 'User was successfully authenticated.'
-      session[:user_id] = user.id
-    else
-      flash[:danger] = 'Could not authenticate user.'
-    end
-
-    redirect_to root_path
-  end
+    handle_auth
 end
