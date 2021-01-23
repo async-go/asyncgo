5ce8d44f59f827ae7db0c0fabde71f9016517fc5
Jason Yavorska <jason@gitlab.com>
2021-01-23 09:56:50 +0100

    Add model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.not_to allow_value("", " ").for(:name) }
+    it { is_expected.to allow_value(nil).for(:name) }
   end
 
   describe 'Relations' do
