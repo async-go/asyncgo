aa11c3220bc12cad88e347d27b78dc554791010f
Jason Yavorska <jason@asyncgo.com>
2021-01-24 09:56:52 +0100

    Helper for displaying due date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def printable_due_date(topic)
+    if topic.due_date.present?
+      if topic.days_remaining.negative?
+        "#{pluralize(topic.days_remaining.abs, 'day')} overdue"
+      else
+        "#{pluralize(topic.days_remaining, 'day')} remaining"
+      end
+    else
+      'No due date set'
+    end
+  end
 end
