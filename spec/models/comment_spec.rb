b1c1f58fac6c76f6ae36c5fe40e31d775e501d60
Jason Yavorska <jason@gitlab.com>
2021-01-25 17:21:10 +0100

    Re-add validation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Comment, type: :model do
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:body) }
+  end
+
   describe 'Relations' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:topic) }
