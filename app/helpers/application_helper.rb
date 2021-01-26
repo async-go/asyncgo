3d28530967c1d8624db248795f0649f7bb7cbb63
Matija Čupić <matteeyah@gmail.com>
2021-01-26 13:42:02 +0100

    Use FontAwesome notification icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
   def notification_text(notification)
     "#{notification.actor.printable_name} #{notification.action} an #{notification.target.class.to_s.downcase}"
   end
+
+  def active_link_to(text, path, class_names)
+    link_to text, path, class: class_names(class_names, active: current_page?(path))
+  end
 end
