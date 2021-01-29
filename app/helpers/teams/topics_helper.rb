ee2d55bfe2452847b668fee3195bbba9cd20b405
Jason Yavorska <jason@gitlab.com>
2021-01-29 17:23:15 +0100

    Count due date at end of day

--- app/helpers/teams/topics_helper.rb
+++ app/helpers/teams/topics_helper.rb
@@ -10,10 +10,10 @@ module Teams
       return 'No due date' unless topic.due_date?
 
       if topic.active?
-        if topic.due_date < Time.now.utc
-          "Due #{distance_of_time_in_words(topic.due_date, Time.now.utc)} ago"
+        if topic.due_date.end_of_day < Time.now.utc
+          "Due #{distance_of_time_in_words(topic.due_date.end_of_day, Time.now.utc)} ago"
         else
-          "Due in #{distance_of_time_in_words(Time.now.utc, topic.due_date)}"
+          "Due in #{distance_of_time_in_words(Time.now.utc, topic.due_date.end_of_day)}"
         end
       else
         "Due #{topic.due_date.strftime('%b %-d')}"
--- spec/helpers/teams/topics_helper_spec.rb
+++ spec/helpers/teams/topics_helper_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Teams::TopicsHelper, type: :helper do
 
     context 'when topic has due date and is active' do
       before do
-        topic.due_date = Time.utc(2020, 1, 1)
+        topic.due_date = Time.utc(2020, 1, 1).end_of_day
         topic.status = :active
       end
 
@@ -52,7 +52,7 @@ RSpec.describe Teams::TopicsHelper, type: :helper do
           travel_back
         end
 
-        it { is_expected.to eq('Due 1 day ago') }
+        it { is_expected.to eq('Due less than a minute ago') }
       end
 
       context 'when topic is not overdue' do
@@ -64,7 +64,7 @@ RSpec.describe Teams::TopicsHelper, type: :helper do
           travel_back
         end
 
-        it { is_expected.to eq('Due in 2 days') }
+        it { is_expected.to eq('Due in 3 days') }
       end
     end
 
