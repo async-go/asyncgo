42f56fe998c59db39372d721933d63903f4c520a
Jason Yavorska <jason@asyncgo.com>
2021-01-29 07:55:08 +0100

    Don't show clear notifications if there aren't any

--- spec/system/notifications_spec.rb
+++ spec/system/notifications_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe 'Notifications', type: :system do
 
     expect(find('#notificationDropdown')).to have_text('1')
     find('.dropdown-toggle.badge').click
-    click_link '(clear)'
+    click_link '✅ Clear all notifications'
     expect(find('#notificationDropdown')).to have_text('0')
   end
 end
