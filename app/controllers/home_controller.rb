a3abe7a2102709a5dc6405f33b57c2ecce93d177
Jason Yavorska <jason@asyncgo.com>
2021-01-31 10:17:58 +0100

    Add logged in redirect for topic list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if current_user
+      redirect_to team_topics_path(current_user)
+    end
+  end
 end
