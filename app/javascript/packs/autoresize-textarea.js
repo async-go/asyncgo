d8ca2d7593504e308e0d83146307864fb0d40cc8
Jason Yavorska <jason@gitlab.com>
2021-02-03 11:32:23 +0100

    Simplify event

--- app/javascript/packs/autoresize-textarea.js
+++ app/javascript/packs/autoresize-textarea.js
@@ -1,16 +1,15 @@
-function addAutoResize() {
-    document.querySelectorAll('.autoresize').forEach(function (element) {
-        element.style.boxSizing = 'border-box';
-        element.style.height = element.scrollHeight + offset + 'px';
-        var offset = element.offsetHeight - element.clientHeight;
-        element.addEventListener('input', function (event) {
-            event.target.style.height = 'auto';
-            event.target.style.height = event.target.scrollHeight + offset + 'px';
-        });
-        element.removeAttribute('autoresize');
-        var event = new Event('input')
-        element.dispatchEvent(event);
-    });
+function addAutoResize () {
+  document.querySelectorAll('.autoresize').forEach(function (element) {
+    element.style.boxSizing = 'border-box'
+    const offset = element.offsetHeight - element.clientHeight
+    element.style.height = element.scrollHeight + offset + 'px'
+    element.addEventListener('input', function (event) {
+      event.target.style.height = 'auto'
+      event.target.style.height = event.target.scrollHeight + offset + 'px'
+    })
+    element.removeAttribute('autoresize')
+    element.dispatchEvent(new Event('input'))
+  })
 }
 
-addAutoResize();
+addAutoResize()
