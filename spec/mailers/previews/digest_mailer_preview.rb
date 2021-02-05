b70b4f2c9d37e70e6f700f0089803c6ccc679295
Jason Yavorska <jason@asyncgo.com>
2021-02-05 07:22:52 +0100

    Remove unused preview bits

--- spec/mailers/previews/digest_mailer_preview.rb
+++ spec/mailers/previews/digest_mailer_preview.rb
@@ -5,10 +5,7 @@ class DigestMailerPreview < ActionMailer::Preview
   def digest_email
     team = Team.new(name: 'sample team')
     user = User.new(id: 1, email: 'test@example.com', name: 'Bob Test', team: team)
-    actor = User.new(email: 'actor@example.com', name: 'John Doe', team: team)
-    topic = Topic.new(title: 'example topic', team: team, user: actor)
-    notifications = [Notification.new(id: 1, target: topic, user: user, actor: actor, action: :created)]
 
-    DigestMailer.with(user: user, notifications: notifications).digest_email
+    DigestMailer.with(user: user).digest_email
   end
 end
