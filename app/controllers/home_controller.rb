d8d470c21f14f85eaa39a863e31a012a7aad649f
Jason Y <jason@asyncgo.com>
2021-02-06 01:38:09 +0100

    Add logged in redirect for topic list (#221)
    * Add logged in redirect for topic list
    
    * Remove message you will never see
    
    * Rubocop fix
    
    * Check for team also
    
    * Rubocop correction
    
    * Fix issue with redirect
    
    * Make home redirect explicit
    
    Co-authored-by: Matija Čupić <matteeyah@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    if current_user&.team
+      redirect_to team_topics_path(current_user.team)
+    else
+      render :index
+    end
+  end
 end
