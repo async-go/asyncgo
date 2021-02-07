fbedb9ae1dee110a418d3650353126eeeb2b9bce
Jason Yavorska <jason@asyncgo.com>
2021-02-07 06:39:56 +0100

    Rework notification specs

--- spec/system/notifications_spec.rb
+++ spec/system/notifications_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Notifications', type: :system do
     click_link 'Topics'
     click_link 'New Topic'
     fill_in 'topic[title]', with: 'Sample topic'
-    fill_in 'markdown_description', with: 'Sample topic description'
+    fill_in 'topic[description]', with: 'Sample topic description'
     click_button 'Create Topic'
 
     click_link 'Sign out'
@@ -40,7 +40,7 @@ RSpec.describe 'Notifications', type: :system do
     click_link 'Topics'
     click_link 'Sample topic'
 
-    fill_in 'markdown_body', with: '__Sample content__'
+    fill_in 'comment[body]', with: '__Sample content__'
     click_button 'Add Comment'
 
     click_link 'Sign out'
