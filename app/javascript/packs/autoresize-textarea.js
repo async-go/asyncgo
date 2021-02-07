f28f5beb18d4773a72b06e38e1219bc9c2f0af5b
Matija Čupić <matteeyah@gmail.com>
2021-02-07 01:22:21 +0100

    Add support for page specific javascripts

--- app/javascript/packs/autoresize-textarea.js
+++ app/javascript/packs/autoresize-textarea.js
@@ -1,17 +1,11 @@
-/* global Event */
-
-function addAutoResize () {
-  document.querySelectorAll('.autoresize').forEach(function (element) {
-    element.style.boxSizing = 'border-box'
-    const offset = element.offsetHeight - element.clientHeight
-    element.style.height = element.scrollHeight + offset + 'px'
-    element.addEventListener('input', function (event) {
-      event.target.style.height = 'auto'
-      event.target.style.height = event.target.scrollHeight + offset + 'px'
-    })
-    element.removeAttribute('autoresize')
-    element.dispatchEvent(new Event('input'))
+document.querySelectorAll('.autoresize').forEach(function (element) {
+  element.style.boxSizing = 'border-box'
+  const offset = element.offsetHeight - element.clientHeight
+  element.style.height = element.scrollHeight + offset + 'px'
+  element.addEventListener('input', function (event) {
+    event.target.style.height = 'auto'
+    event.target.style.height = event.target.scrollHeight + offset + 'px'
   })
-}
-
-addAutoResize()
+  element.removeAttribute('autoresize')
+  element.dispatchEvent(new Event('input'))
+})
