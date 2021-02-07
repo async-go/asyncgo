ef79de141317e5daff84f24aa1105b9b5dcd4f4d
Matija Čupić <matteeyah@gmail.com>
2021-02-07 01:59:29 +0100

    Simplify auto resize text area

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -1,11 +1,7 @@
 document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
-  element.style.boxSizing = 'border-box'
-  const offset = element.offsetHeight - element.clientHeight
-  element.style.height = element.scrollHeight + offset + 'px'
+  element.style.height = element.scrollHeight + 'px'
   element.addEventListener('input', function (event) {
     event.target.style.height = 'auto'
-    event.target.style.height = event.target.scrollHeight + offset + 'px'
+    event.target.style.height = event.target.scrollHeight + 'px'
   })
-  element.removeAttribute('text-area-autoresize')
-  element.dispatchEvent(new Event('input'))
 })
