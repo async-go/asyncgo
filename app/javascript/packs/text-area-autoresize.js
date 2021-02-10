d08b34337b49564e4671f0d4c9f84cb209034db8
Matija Čupić <matteeyah@gmail.com>
2021-02-10 18:07:39 +0100

    Do not use trubolinks:load in javascript (#262)

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -1,9 +1,7 @@
-document.addEventListener('turbolinks:load', function () {
-  document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
-    element.style.height = element.scrollHeight + 'px'
-    element.addEventListener('input', function (event) {
-      event.target.style.height = 'auto'
-      event.target.style.height = event.target.scrollHeight + 'px'
-    })
+document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
+  element.style.height = element.scrollHeight + 'px'
+  element.addEventListener('input', function (event) {
+    event.target.style.height = 'auto'
+    event.target.style.height = event.target.scrollHeight + 'px'
   })
 })
