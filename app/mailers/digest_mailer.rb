4f58c88d11929839044420683fde22b0a3f67550
Jason Yavorska <jason@metaluna.io>
2021-02-10 08:13:32 +0100

    Don't send empty notification

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -7,6 +7,9 @@ class DigestMailer < ApplicationMailer
   def digest_email
     @notifications = params[:user].notifications.where(read_at: nil)
 
-    mail(to: params[:user].email, subject: 'Your AsyncGo Digest', notifications: @notifications)
+    return if @notifications.empty?
+
+    mail(to: params[:user].email, subject: 'Your AsyncGo Digest',
+         notifications: @notifications)
   end
 end
