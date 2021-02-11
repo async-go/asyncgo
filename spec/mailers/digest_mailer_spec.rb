7dc9dc3d02d24891d90e0b29ecdf280881329351
Jason Yavorska <jason@metaluna.io>
2021-02-11 09:50:18 +0100

    Rubocop fixes

--- spec/mailers/digest_mailer_spec.rb
+++ spec/mailers/digest_mailer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe DigestMailer, type: :mailer do
   let!(:notification) { FactoryBot.create(:notification, user: user) }
 
   describe '#digest_email' do
-    subject(:digest_email) { described_class.with(user: user, notifications: [ notification ] ).digest_email }
+    subject(:digest_email) { described_class.with(user: user, notifications: [notification]).digest_email }
 
     it 'renders the headers' do
       expect(digest_email).to have_attributes(
