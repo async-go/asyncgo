e17bd5164179dd3e6e29197d94a330a4760b4bd5
Jason Yavorska <jason@metaluna.io>
2021-02-11 09:55:35 +0100

    Remove abort function

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -4,16 +4,8 @@ class DigestMailer < ApplicationMailer
   helper ApplicationHelper
   default from: 'notifications@asyncgo.com'
 
-  after_action :prevent_delivery_if_no_notifications
-
   def digest_email
     @notifications = params[:notifications]
     mail(to: params[:user].email, subject: 'Your AsyncGo Digest', notifications: @notifications)
-  end
-
-  private
-
-  def prevent_delivery_if_no_notifications
-    mail.perform_deliveries = false if @notifications.count.zero?
   end
 end
