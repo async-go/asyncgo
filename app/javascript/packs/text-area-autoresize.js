40114d85795ccb74ab674853c21a54b35b733a2d
Jason Yavorska <jason@metaluna.io>
2021-02-12 15:26:47 +0100

    Never shrink

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -2,10 +2,9 @@ document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
     element.style.height = element.scrollHeight + 'px'
     element.addEventListener('input', function (event) {
-      if (parseInt(event.target.style.height, 10) > event.target.scrollHeight) {
-        event.target.style.height = 'auto'
+      if (parseInt(event.target.style.height, 10) <= event.target.scrollHeight) {
+        event.target.style.height = event.target.scrollHeight + 'px'
       }
-      event.target.style.height = event.target.scrollHeight + 'px'
     })
   })
 })
