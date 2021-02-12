1b13b73b63c5d10c846cbfaa662f2b055f3c538c
Jason Yavorska <jason@metaluna.io>
2021-02-12 09:46:14 +0100

    Linter fixes

--- Gemfile
+++ Gemfile
@@ -75,4 +75,4 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
-gem "tribute", "~> 3.6"
+gem 'tribute', '~> 3.6'
--- app/javascript/packs/tribute.js
+++ app/javascript/packs/tribute.js
@@ -1,9 +1,9 @@
-import Tribute from "tributejs"
-console.log("Hello")
-var tribute = new Tribute({
-values: [
-        { key: "Phil Heartman", value: "Phil_Heartman" },
-        { key: "Gordon Ramsey", value: "Gordon_Ramsey" }
-    ]
-    });
-tribute.attach(document.querySelectorAll(".mentionable"));  
+import Tribute from 'tributejs'
+const tribute = new Tribute({
+  values: [
+    { key: 'Phil Heartman', value: 'Phil_Heartman' },
+    { key: 'Gordon Ramsey', value: 'Gordon_Ramsey' }
+  ]
+})
+
+tribute.attach(document.querySelectorAll('.mentionable'))
