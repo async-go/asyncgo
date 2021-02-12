615173efece9d9a1ed887fb9e48c873a9d4c68fb
Jason Yavorska <jason@metaluna.io>
2021-02-12 14:46:27 +0100

    Simplify logic

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -2,12 +2,10 @@ document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
     element.style.height = element.scrollHeight + 'px'
     element.addEventListener('input', function (event) {
-      if (parseInt(event.target.style.height, 10) <= event.target.scrollHeight) {
-        event.target.style.height = event.target.scrollHeight + 'px'
-      } else {
+      if (parseInt(event.target.style.height, 10) > event.target.scrollHeight) {
         event.target.style.height = 'auto'
-        event.target.style.height = event.target.scrollHeight + 'px'
       }
+      event.target.style.height = event.target.scrollHeight + 'px'
     })
   })
 })
