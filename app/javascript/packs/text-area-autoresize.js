12981f8c8d8a71b940a927b8999f754c4f4dc2f7
Matija Čupić <matteeyah@gmail.com>
2021-02-12 15:54:25 +0100

    Use external library for resizing

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -1,10 +1,7 @@
+import autosize from 'autosize'
+
 document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
-    element.style.height = element.scrollHeight + 'px'
-    element.addEventListener('input', function (event) {
-      if (parseInt(event.target.style.height, 10) <= event.target.scrollHeight) {
-        event.target.style.height = event.target.scrollHeight + 'px'
-      }
-    })
+    autosize(element)
   })
 })
