8c007a03243892af70f0b0c59f058a39b3aceb77
Jason Yavorska <jason@metaluna.io>
2021-02-12 14:44:04 +0100

    Stop snapping on resize

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -2,8 +2,12 @@ document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
     element.style.height = element.scrollHeight + 'px'
     element.addEventListener('input', function (event) {
-      event.target.style.height = 'auto'
-      event.target.style.height = event.target.scrollHeight + 'px'
+      if (parseInt(event.target.style.height, 10) <= event.target.scrollHeight) {
+        event.target.style.height = event.target.scrollHeight + 'px'
+      } else {
+        event.target.style.height = 'auto'
+        event.target.style.height = event.target.scrollHeight + 'px'
+      }
     })
   })
 })
