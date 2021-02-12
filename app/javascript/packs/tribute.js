ce492530d79963600ebf53a4f61a0f12ddac951c
Jason Yavorska <jason@metaluna.io>
2021-02-12 09:42:28 +0100

    Use underscores for value

--- app/javascript/packs/tribute.js
+++ app/javascript/packs/tribute.js
@@ -2,8 +2,8 @@ import Tribute from "tributejs"
 console.log("Hello")
 var tribute = new Tribute({
 values: [
-        { key: "Phil Heartman", value: "pheartman" },
-        { key: "Gordon Ramsey", value: "gramsey" }
+        { key: "Phil Heartman", value: "Phil_Heartman" },
+        { key: "Gordon Ramsey", value: "Gordon_Ramsey" }
     ]
     });
 tribute.attach(document.querySelectorAll(".mentionable"));  
