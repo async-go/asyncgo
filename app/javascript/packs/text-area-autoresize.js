1ca7ebb92876407f5f8c57809d4eb1528f3acfe5
Matija Čupić <matteeyah@gmail.com>
2021-02-12 17:13:58 +0100

    Scroll up to view height

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -1,7 +1,14 @@
-import autosize from 'autosize'
-
 document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
-    autosize(element)
+    element.style.height = element.scrollHeight + 'px'
+    element.addEventListener('input', function (event) {
+      const previousHeight = event.target.style.height
+      event.target.style.height = 'auto'
+      if (event.target.scrollHeight < 320) {
+        event.target.style.height = event.target.scrollHeight + 'px'
+      } else {
+        event.target.style.height = previousHeight
+      }
+    })
   })
 })
