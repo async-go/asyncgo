3b3e66368e37d40cb56709515988dd7d61a98aeb
Jason Yavorska <jason@metaluna.io>
2021-02-13 11:48:23 +0100

    More fixes for autoresize

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -1,13 +1,20 @@
 document.addEventListener('turbolinks:load', function () {
   document.querySelectorAll('.text-area-autoresize').forEach(function (element) {
-    element.style.height = element.scrollHeight + 'px'
+    if (element.scrollHeight < (window.innerHeight * .6)) {
+      element.style.height = 'auto'
+      element.style.height = element.scrollHeight + 'px'
+    } else {
+      element.style.height = 'auto'
+      element.style.height = (window.innerHeight * .6) + 'px'
+    }
     element.addEventListener('input', function (event) {
-      const previousHeight = event.target.style.height
-      event.target.style.height = 'auto'
-      if (event.target.scrollHeight < 320) {
+      console.log(event.target.scrollHeight)
+      if (event.target.scrollHeight < (window.innerHeight * .6)) {
+        event.target.style.height = 'auto'
         event.target.style.height = event.target.scrollHeight + 'px'
       } else {
-        event.target.style.height = previousHeight
+        event.target.style.height = 'auto'
+        event.target.style.height = (window.innerHeight * .6) + 'px'
       }
     })
   })
