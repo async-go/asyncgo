515b43decf2cd5dc04fbc0e36a934ccf73570c90
Jason Yavorska <jason@metaluna.io>
2021-02-13 11:49:57 +0100

    Remove logging

--- app/javascript/packs/text-area-autoresize.js
+++ app/javascript/packs/text-area-autoresize.js
@@ -8,7 +8,6 @@ document.addEventListener('turbolinks:load', function () {
       element.style.height = (window.innerHeight * .6) + 'px'
     }
     element.addEventListener('input', function (event) {
-      console.log(event.target.scrollHeight)
       if (event.target.scrollHeight < (window.innerHeight * .6)) {
         event.target.style.height = 'auto'
         event.target.style.height = event.target.scrollHeight + 'px'
