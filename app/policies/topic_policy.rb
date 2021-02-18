333b3a5fe805bc1afb1f0729f9b64f8a3c5e4313
Matija Čupić <matteeyah@gmail.com>
2021-02-18 01:48:29 +0100

    Flatten policy structure (#290)

---
+++ app/policies/topic_policy.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class TopicPolicy < ApplicationPolicy
+  def index?
+    user &&
+      record == user.team
+  end
+
+  def show?
+    user &&
+      record.team == user.team
+  end
+
+  def new?
+    user &&
+      record.team == user.team
+  end
+
+  def edit?
+    user &&
+      record.team == user.team &&
+      record.active?
+  end
+
+  def create?
+    user &&
+      record.team == user.team
+  end
+
+  def update?
+    user &&
+      record.team == user.team
+  end
+
+  def subscribe?
+    user &&
+      record.team == user.team
+  end
+end
