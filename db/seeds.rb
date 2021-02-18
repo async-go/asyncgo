d165778661fc776e039aa35bd930c08f0400031c
Jason Yavorska <jason@metaluna.io>
2021-02-18 14:53:36 +0100

    Get seeds working

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,52 +12,55 @@
 asyncgo = Team.create(name: 'AsyncGo')
 
 # Users
-jason = User.create(name: 'Jason Yavorska', email: 'jason@asyncgo.com', team: asyncgo)
-matija = User.create(name: 'Matija Cupic', email: 'matija@asyncgo.com', team: asyncgo)
-bob = User.create(name: 'Bob Tester', email: 'testdata-bob@asyncgo.com', team: asyncgo)
-User.create(name: 'Larry Sample', email: 'testdata-larry@asyncgo.com')
+jason = User.create(id: 0, name: 'Jason Yavorska', email: 'jason@asyncgo.com', team: asyncgo)
+matija = User.create(id: 1, name: 'Matija Cupic', email: 'matija@asyncgo.com', team: asyncgo)
+bob = User.create(id: 2, name: 'Bob Tester', email: 'testdata-bob@asyncgo.com', team: asyncgo)
+alice = User.create(id: 3, name: 'Alice Sample', email: 'testdata-alice@asyncgo.com', team: asyncgo)
 
 # Topic
 topic = Topic.create(
-  user: jason, team: asyncgo, title: 'Daily standup', due_date: Time.zone.today,
+  user: bob, team: asyncgo, title: 'Daily standup', due_date: Time.zone.today,
   description: '- Hello', description_html: '<ul><li>Hello</li></ul>'
 )
 
 # Topic Votes
-Vote.create(user: matija, votable: topic, emoji: 'thumbsup')
+#Vote.create(user: matija, votable: topic, emoji: 'thumbsup')
 
 # Comment
 comment = Comment.create(
-  user: matija, topic: topic, body: 'No update from me today', body_html: 'No update from me today'
+  user: alice, topic: topic, body: 'No update from me today', body_html: 'No update from me today'
 )
 
 # Comment Votes
-Vote.create(user: jason, votable: comment, emoji: 'thumbsdown')
+#Vote.create(user: alice, votable: comment, emoji: 'thumbsdown')
 
 # Subscriptions
 Subscription.create(topic: topic, user: matija)
+Subscription.create(topic: topic, user: jason)
 
 # Notifications
-Notification.create(user: jason, actor: matija, action: :created, target: comment)
-Notification.create(user: jason, actor: matija, action: :updated, target: topic)
-Notification.create(user: matija, actor: jason, action: :updated, target: topic)
+Notification.create(user: jason, actor: bob, action: :created, target: comment)
+Notification.create(user: jason, actor: alice, action: :updated, target: topic)
+Notification.create(user: matija, actor: bob, action: :created, target: comment)
+Notification.create(user: matija, actor: alice, action: :updated, target: topic)
+
 
 # Topics
 Topic.create(user: bob, title: 'Lets discuss our next marketing campaign',
              description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
              due_date: Time.zone.today + 5)
-Topic.create(user: bob, title: 'Competitive analysis vs. our top competitor',
+Topic.create(user: alice, title: 'Competitive analysis vs. our top competitor',
              description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
              outcome: 'We are the best', outcome_html: '<p>We are the best</p>',
              due_date: Time.zone.today - 2)
 Topic.create(user: bob, title: 'Discuss possible next big features',
              description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
              due_date: Time.zone.today + 4)
-Topic.create(user: bob, title: 'Feedback on our latest demo',
+Topic.create(user: alice, title: 'Feedback on our latest demo',
              description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
              outcome: 'Action items', outcome_html: '<p>Action items</p>',
              due_date: Time.zone.today)
-Topic.create(user: bob, title: 'Review general customer feedback so far',
+Topic.create(user: alice, title: 'Review general customer feedback so far',
              description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
              outcome: 'All good so far', outcome_html: '<p>All good so far</p>',
              due_date: nil)
