37a9a988f7c6d5bab3e63cbaa65100e8208c1725
Jason Y <jason@asyncgo.com>
2021-02-18 21:27:05 +0100

    Improve seeds to have actions by fake users (#297)

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,35 +27,37 @@ bob = User.new(name: 'Bob Tester', email: 'testdata-bob@asyncgo.com', team: asyn
   user_bob.save!
 end
 
-User.new(name: 'Larry Sample', email: 'testdata-larry@asyncgo.com').tap do |user_larry|
-  user_larry.preference = user_larry.build_preference
-  user_larry.save!
+alice = User.new(name: 'Alice Samples', email: 'testdata-alice@asyncgo.com', team: asyncgo).tap do |user_alice|
+  user_alice.preference = user_alice.build_preference
+  user_alice.save!
 end
 
 # Topic
 topic = Topic.create!(
-  user: jason, team: asyncgo, title: 'Daily standup', due_date: Time.zone.today,
+  user: alice, team: asyncgo, title: 'Daily standup', due_date: Time.zone.today,
   description: '- Hello', description_html: '<ul><li>Hello</li></ul>'
 )
 
 # Topic Votes
-Vote.create!(user: matija, votable: topic, emoji: 'thumbsup')
+Vote.create!(user: bob, votable: topic, emoji: 'thumbsup')
 
 # Comment
 comment = Comment.create!(
-  user: matija, topic: topic, body: 'No update from me today', body_html: 'No update from me today'
+  user: bob, topic: topic, body: 'No update from me today', body_html: 'No update from me today'
 )
 
 # Comment Votes
-Vote.create!(user: jason, votable: comment, emoji: 'thumbsdown')
+Vote.create!(user: alice, votable: comment, emoji: 'thumbsdown')
 
 # Subscriptions
 Subscription.create!(topic: topic, user: matija)
+Subscription.create!(topic: topic, user: jason)
 
 # Notifications
-Notification.create!(user: jason, actor: matija, action: :created, target: comment)
-Notification.create!(user: jason, actor: matija, action: :updated, target: topic)
-Notification.create!(user: matija, actor: jason, action: :updated, target: topic)
+Notification.create!(user: jason, actor: bob, action: :created, target: comment)
+Notification.create!(user: jason, actor: alice, action: :updated, target: topic)
+Notification.create!(user: matija, actor: bob, action: :updated, target: topic)
+Notification.create!(user: matija, actor: alice, action: :updated, target: topic)
 
 # Topics
 Topic.create!(user: bob, title: 'Lets discuss our next marketing campaign',
@@ -68,11 +70,11 @@ Topic.create!(user: bob, title: 'Competitive analysis vs. our top competitor',
 Topic.create!(user: bob, title: 'Discuss possible next big features',
               description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
               team: asyncgo, due_date: Time.zone.today + 4)
-Topic.create!(user: bob, title: 'Feedback on our latest demo',
+Topic.create!(user: alice, title: 'Feedback on our latest demo',
               description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
               outcome: 'Action items', outcome_html: '<p>Action items</p>',
               team: asyncgo, due_date: Time.zone.today)
-Topic.create!(user: bob, title: 'Review general customer feedback so far',
+Topic.create!(user: alice, title: 'Review general customer feedback so far',
               description: '- Hello', description_html: '<ul><li>Hello</li></ul>',
               outcome: 'All good so far', outcome_html: '<p>All good so far</p>',
               team: asyncgo, due_date: nil)
