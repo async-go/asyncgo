3af09040ebe07a3fe7205a9b9a68e66c275ca00a
Matija Čupić <matteeyah@gmail.com>
2021-02-28 16:23:36 +0100

    Move #emoji_group_text helper to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
   def assistive_icon(source, icon, title, classname: nil)
     icon(source, icon, class: classname) + tag.span(title, class: 'visually-hidden')
   end
+
+  def emoji_group_text(emoji_name, count)
+    "#{Emoji.find_by_alias(emoji_name).raw} #{count}" # rubocop:disable Rails/DynamicFindBy
+  end
 end
--- spec/helpers/teams/topics_helper_spec.rb
+++ spec/helpers/teams/topics_helper_spec.rb
@@ -124,10 +124,4 @@ RSpec.describe Teams::TopicsHelper, type: :helper do
       end
     end
   end
-
-  describe '#emoji_group_text' do
-    subject(:emoji_group_text) { helper.emoji_group_text('cat', 2) }
-
-    it { is_expected.to eq("#{Emoji.find_by_alias('cat').raw} 2") } # rubocop:disable Rails/DynamicFindBy
-  end
 end
