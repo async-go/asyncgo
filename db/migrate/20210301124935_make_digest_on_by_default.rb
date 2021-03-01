5467993f8723a278c1371822c229b959c6237a17
Jason Yavorska <jason@metaluna.io>
2021-03-01 13:57:57 +0100

    Rubocop

--- db/migrate/20210301124935_make_digest_on_by_default.rb
+++ db/migrate/20210301124935_make_digest_on_by_default.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MakeDigestOnByDefault < ActiveRecord::Migration[6.1]
   def change
     change_column(:user_preferences, :digest_enabled, :boolean, default: true, null: false)
