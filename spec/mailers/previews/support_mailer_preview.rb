4bec7892e67d35176fe0a46a353211367af818ad
Matija Čupić <matteeyah@gmail.com>
2021-03-25 12:32:44 +0100

    Add more information to support requests (#395)
    * Add more information to support mailer
    
    * Fix digest mailer preview
    
    * Fix failing test

--- spec/mailers/previews/support_mailer_preview.rb
+++ spec/mailers/previews/support_mailer_preview.rb
@@ -3,7 +3,7 @@
 class SupportMailerPreview < ActionMailer::Preview
   def support_email
     team = Team.new(id: 1, name: 'example team')
-    user = User.new(email: 'test@example.com', team: team)
+    user = User.new(id: 1, email: 'test@example.com', team: team)
     body = 'Sample body'
 
     SupportMailer.with(user: user, body: body).support_email
