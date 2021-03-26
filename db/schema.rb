2d4427ac943b6d1c4574693694d6cc70a8f7b2d8
Jason Y <jason@asyncgo.com>
2021-03-26 08:24:27 +0100

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2021_03_26_064951) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "blazer_audits", force: :cascade do |t|
