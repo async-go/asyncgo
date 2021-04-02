ea95372e623d00a1d3a79dabee99f4fbe091b449
Matija Čupić <matteeyah@gmail.com>
2021-04-02 23:30:28 +0200

    Stub Slack OAuth flow

--- spec/requests/omniauth_callbacks_controller_spec.rb
+++ spec/requests/omniauth_callbacks_controller_spec.rb
@@ -1,78 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe OmniauthCallbacksController, type: :request do
-  describe 'POST slack' do
-    subject(:post_slack) do
-      post '/auth/slack'
-      follow_redirect!
-    end
-
-    let(:auth_hash) do
-    {
-      user: {
-        name: 'Sonny Whether',
-        email: 'bobby@example.com'
-      }
-    }
-    end
-
-    before do
-      OmniAuth.config.add_mock(:slack, auth_hash)
-    end
-
-    context 'when user does not exist' do
-      it 'creates the user' do
-        expect { post_slack }.to change(User, :count).from(0).to(1)
-      end
-
-      it 'signs the user in' do
-        post_slack
-
-        expect(controller.send(:current_user)).not_to eq(nil)
-      end
-
-      it 'updates the users name' do
-        post_slack
-
-        expect(User.last.name).to eq('John Sample')
-      end
-
-      it 'sets the flash' do
-        post_slack
-
-        expect(controller.flash[:success]).to eq('User was successfully authenticated.')
-      end
-
-      it 'redirects to redirect_uri' do
-        expect(post_slack).to redirect_to(root_path)
-      end
-    end
-
-    context 'when the user exists' do
-      let!(:user) { FactoryBot.create(:user, email: 'john@example.com') }
-
-      it 'signs the user in' do
-        post_slack
-
-        expect(controller.send(:current_user)).to eq(user)
-      end
-
-      it 'updates the users name' do
-        expect { post_slack }.to change { user.reload.name }.from(nil).to('John Sample')
-      end
-
-      it 'sets the flash' do
-        post_slack
-
-        expect(controller.flash[:success]).to eq('User was successfully authenticated.')
-      end
-
-      it 'redirects to redirect_uri' do
-        expect(post_slack).to redirect_to(root_path)
-      end
-    end
-  end
-
   describe 'POST google_oauth2' do
     subject(:post_google_oauth2) do
       post '/auth/google_oauth2'
@@ -216,6 +144,84 @@ RSpec.describe OmniauthCallbacksController, type: :request do
 
       it 'redirects to redirect_uri' do
         expect(post_github).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe 'POST slack' do
+    subject(:post_slack) do
+      post '/auth/slack'
+      follow_redirect!
+    end
+
+    let(:slack_omniauth_strategy) do
+      { authed_user: { token: 'test' } }
+    end
+
+    before do
+      strategy_double = instance_double(
+        OmniAuth::Slack::OAuth2::AccessToken,
+        authed_user: OpenStruct.new({ token: 'test' })
+      )
+      allow_any_instance_of(OmniAuth::Strategies::Slack).to receive(:access_token).and_return(strategy_double) # rubocop:disable RSpec/AnyInstance
+
+      access_token_double = instance_double(
+        OmniAuth::Slack::OAuth2::AccessToken, get: instance_double(
+          OAuth2::Response, parsed: { 'user' => { 'email' => 'john@example.com', 'name' => 'John Sample' } }
+        )
+      )
+      allow(OmniAuth::Slack).to receive(:build_access_token).and_return(access_token_double)
+    end
+
+    context 'when user does not exist' do
+      it 'creates the user' do
+        expect { post_slack }.to change(User, :count).from(0).to(1)
+      end
+
+      it 'signs the user in' do
+        post_slack
+
+        expect(controller.send(:current_user)).not_to eq(nil)
+      end
+
+      it 'updates the users name' do
+        post_slack
+
+        expect(User.last.name).to eq('John Sample')
+      end
+
+      it 'sets the flash' do
+        post_slack
+
+        expect(controller.flash[:success]).to eq('User was successfully authenticated.')
+      end
+
+      it 'redirects to redirect_uri' do
+        expect(post_slack).to redirect_to(root_path)
+      end
+    end
+
+    context 'when the user exists' do
+      let!(:user) { FactoryBot.create(:user, email: 'john@example.com') }
+
+      it 'signs the user in' do
+        post_slack
+
+        expect(controller.send(:current_user)).to eq(user)
+      end
+
+      it 'updates the users name' do
+        expect { post_slack }.to change { user.reload.name }.from(nil).to('John Sample')
+      end
+
+      it 'sets the flash' do
+        post_slack
+
+        expect(controller.flash[:success]).to eq('User was successfully authenticated.')
+      end
+
+      it 'redirects to redirect_uri' do
+        expect(post_slack).to redirect_to(root_path)
       end
     end
   end
