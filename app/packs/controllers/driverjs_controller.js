81a968993cc1febb4a8b13fe2ae0cca4894ebc75
Jason Yavorska <jason@asyncgo.com>
2021-04-03 11:42:36 +0200

    Make logging more clear

--- app/packs/controllers/driverjs_controller.js
+++ app/packs/controllers/driverjs_controller.js
@@ -2,8 +2,13 @@ import { Controller } from 'stimulus'
 import Driver from 'driver.js/dist/driver.min.js'
 
 export default class extends Controller {
+
+  connect () {
+    console.log('Hello connect')
+  }
+
   demo () {
-    console.log('Hello')
+    console.log('Hello demo')
     const driver = new Driver()
     driver.highlight({
       element: '#notifications',
