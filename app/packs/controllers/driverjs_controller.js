602faad6a9166291f811b4b14dafffb0f8945974
Jason Yavorska <jason@asyncgo.com>
2021-04-03 11:24:38 +0200

    Move data elements

--- app/packs/controllers/driverjs_controller.js
+++ app/packs/controllers/driverjs_controller.js
@@ -3,21 +3,8 @@ import Driver from 'driver.js/dist/driver.min.js'
 
 
 export default class extends Controller {
-  static values = { users: Array }
 
-  initialize () {
-    const controller = this
-
-    const host = window.location.protocol + '//' + window.location.host
-    const url = `${host}/teams/${window.gon.teamId}/users.json`
-    window.fetch(url)
-      .then(response => response.json())
-      .then(function (data) {
-        controller.usersValue = data
-      })
-  }
-
-  attach () {
+  demo_notification () {
       console.log('Hello');
       const driver = new Driver();
       driver.highlight({
