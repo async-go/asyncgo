0b59dc31296a4f9a88cd6e9f4f172f5fa4ec30f2
Jason Y <jason@asyncgo.com>
2021-04-07 17:04:35 +0200

    Add keyboard shortcuts (#436)
    * Add keyboard shortcuts
    
    * Only have shortcuts on new form
    
    * Remove trailing whitespace
    
    * Simplify topic index hotkeys
    
    * Separate topic_hotkeys from new_form
    
    Co-authored-by: Matija Čupić <matteeyah@gmail.com>

---
+++ app/packs/controllers/hotkey_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from 'stimulus'
+import { install, uninstall } from '@github/hotkey'
+
+export default class extends Controller {
+  connect () {
+    if (this.disabled) return
+    install(this.element)
+  }
+
+  disconnect () {
+    uninstall(this.element)
+  }
+
+  get disabled () {
+    return document.body.hasAttribute('data-hotkeys-disabled')
+  }
+}
