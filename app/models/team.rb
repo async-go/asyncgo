1a92b60859a8a67ce2b3bc7ab8291207661c0f4a
Jason Yavorska <jason@metaluna.io>
2021-04-07 08:20:23 +0200

    Make message more dynamic

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@
 
 class Team < ApplicationRecord
   validates :name, presence: { allow_blank: false, allow_empty: false }
-  validates :message, presence: { allow_blank: false, allow_empty: false, allow_nil: true }
+  validates :message, presence: { allow_blank: true, allow_empty: true }
 
   has_many :users, dependent: :nullify
   has_many :topics, dependent: :destroy
