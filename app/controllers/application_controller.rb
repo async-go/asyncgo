da0ff2e0b15dfda7e930a77f4c75b73d34e121c7
Matija Čupić <matteeyah@gmail.com>
2021-04-11 20:16:33 +0200

    Add Sidekiq Web UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,13 +22,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
-  def authorize_blazer!
-    return if current_user&.email&.ends_with?('@asyncgo.com')
-
-    # This is required because the redirect happens from the Rails engine
-    redirect_to Rails.application.routes.url_helpers.root_path
-  end
-
   def unique_unread_notifications
     @unique_unread_notifications ||= Notification.includes(:actor, :user,
                                                            :target).where(id: notification_grouping_subquery)
