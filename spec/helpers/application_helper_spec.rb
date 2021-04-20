a287378aff59241c9c820a8bc2e7387550ff8469
Jason Yavorska <jason@metaluna.io>
2021-04-20 18:15:35 +0200

    Add test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     it { is_expected.to eq("#{Emoji.find_by_alias('cat').raw} 2") } # rubocop:disable Rails/DynamicFindBy
   end
+
+  describe 'whatsnew_button (within 7 days ago)' do
+    subject(:whatsnew_button_past) { helper.whatsnew_button('https://google.com', Time.zone.today - 6) }
+
+    it { is_expected.to include('btn-outline-success', 'https://google.com') }
+  end
+
+  describe 'whatsnew_button (beyond 7 days ago)' do
+    subject(:whatsnew_button_future) { helper.whatsnew_button('https://google.com', Time.zone.today - 7) }
+
+    it { is_expected.to include('btn-outline-secondary', 'https://google.com') }
+  end
+
 end
