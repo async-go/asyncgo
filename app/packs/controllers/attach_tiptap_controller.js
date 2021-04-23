b738182c9d1dd81589e7b82fa0f14ac5cd61882f
Jason Yavorska <jason@metaluna.io>
2021-04-23 16:17:06 +0200

    Wire up multiple tiptaps

--- app/packs/controllers/attach_tiptap_controller.js
+++ app/packs/controllers/attach_tiptap_controller.js
@@ -5,10 +5,6 @@ import { defaultExtensions } from '@tiptap/starter-kit'
 export default class extends Controller {
     static targets = ['textarea']
 
-    initialize () {
-      window.tiptap = {};
-    }
-
     connect () {
       if (this.textareaTarget.id === "") {
         throw "Error: no ID provided for tiptap editor";
--- app/packs/controllers/index.js
+++ app/packs/controllers/index.js
@@ -7,3 +7,5 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers'
 const application = Application.start()
 const context = require.context('controllers', true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+window.tiptap = {}
