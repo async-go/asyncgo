6d3f4ba57cc8bba09a705c94b008962ff385e22e
Jason Yavorska <jason@metaluna.io>
2021-04-23 08:11:59 +0200

    Remove extra click in initial flow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
   def index
     if current_user&.team
       redirect_to team_topics_path(current_user.team)
+    elsif current_user
+      redirect_to new_team_path
     else
       render :index
     end
