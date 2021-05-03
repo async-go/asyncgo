f72e4a2d12afda8f0592cf402aa9da6d994fb3be
Matija Čupić <matteeyah@gmail.com>
2021-05-03 14:46:48 +0200

    Fix free tier user limit (#520)

--- app/controllers/teams/users_controller.rb
+++ app/controllers/teams/users_controller.rb
@@ -49,7 +49,7 @@ module Teams
     end
 
     def add_user_flash!(team, user)
-      if team.users.count == 5
+      if (team.users.count >= 5) && !team.subscription&.active?
         { danger: 'You have reached the maximum 5 users on the free plan.' }
       elsif !user.valid?
         { danger: "There was a problem adding the user to the team. #{user.errors.full_messages.join(', ')}." }
