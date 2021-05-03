84bb2bb59a06bc85bd714c68da7e8bf768f61636
Matija Čupić <matteeyah@gmail.com>
2021-05-03 15:14:17 +0200

    Fix comment body data migration

--- db/migrate/20210502170632_migrate_comment_body.rb
+++ db/migrate/20210502170632_migrate_comment_body.rb
@@ -18,15 +18,16 @@ class MigrateCommentBody < ActiveRecord::Migration[6.1]
   def up
     Comment.reset_column_information
     Comment.find_each do |comment|
-      comment.update!(content: comment.body)
+      comment.update!(content: comment.body_html)
     end
   end
 
   def down
     Comment.reset_column_information
     Comment.find_each do |comment|
-      content = comment.content.to_plain_text
-      comment.update!(body: content, body_html: "<p>#{content}</p>")
+      plain_content = comment.content.to_plain_text
+      html_content = comment.content.body.to_html
+      comment.update!(body: plain_content, body_html: html_content)
     end
   end
 end
