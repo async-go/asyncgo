a909ca62e6e79384468e4a48119b4e437135539e
Jason Yavorska <jason@metaluna.io>
2021-05-03 17:36:03 +0200

    Open external content in new tab

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -17,3 +17,13 @@ import 'bootstrap'
 
 Rails.start()
 ActiveStorage.start()
+
+// Open all external links in a new window
+addEventListener('click', function (event) {
+  const target = event.target
+  const el = target.closest('a')
+  if (el && !el.isContentEditable && el.host !== window.location.host) {
+    el.setAttribute('target', '_blank')
+    el.setAttribute('rel', 'noopener noreferrer')
+  }
+}, true)
