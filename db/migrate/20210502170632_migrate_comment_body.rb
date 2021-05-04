7e272ab68c7f098f48efe874f0d87fe6f1547784
Matija Čupić <matteeyah@gmail.com>
2021-05-04 02:04:37 +0200

    Revert "Fix comment body data migration (#522)"
    This reverts commit a129d7330e066be3d0af64230eba4ea3ac90cd44.

--- db/migrate/20210502170632_migrate_comment_body.rb
+++ db/migrate/20210502170632_migrate_comment_body.rb
@@ -18,16 +18,15 @@ class MigrateCommentBody < ActiveRecord::Migration[6.1]
   def up
     Comment.reset_column_information
     Comment.find_each do |comment|
-      comment.update!(content: comment.body_html)
+      comment.update!(content: comment.body)
     end
   end
 
   def down
     Comment.reset_column_information
     Comment.find_each do |comment|
-      plain_content = comment.content.to_plain_text
-      html_content = comment.content.body.to_html
-      comment.update!(body: plain_content, body_html: html_content)
+      content = comment.content.to_plain_text
+      comment.update!(body: content, body_html: "<p>#{content}</p>")
     end
   end
 end
