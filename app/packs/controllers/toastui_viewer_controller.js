1fbc9d99706d05bb15bac910540293b12d1828a9
Jason Yavorska <jason@metaluna.io>
2021-05-04 11:10:20 +0200

    Style topics

--- app/packs/controllers/toastui_viewer_controller.js
+++ app/packs/controllers/toastui_viewer_controller.js
@@ -5,6 +5,9 @@ export default class extends Controller {
   static targets = [ "viewer", "content" ]
 
   connect () {
+    console.log("Attaching")
+    console.log(this.viewerTarget)
+    console.log(this.contentTarget)
     const viewer = Editor.factory({
       el: this.viewerTarget,
       viewer: true,
