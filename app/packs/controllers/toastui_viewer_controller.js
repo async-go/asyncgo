4208ec56a691eadc027735291a6c9eb2e2810d21
Jason Yavorska <jason@metaluna.io>
2021-05-04 17:37:37 +0200

    Add markdown preprocessor

--- app/packs/controllers/toastui_viewer_controller.js
+++ app/packs/controllers/toastui_viewer_controller.js
@@ -2,7 +2,7 @@ import { Controller } from 'stimulus'
 import Editor from '@toast-ui/editor'
 
 function preprocessor (markdown) {
-  var mention_regex = /<span class="tribute-mention">(\S+)<\/span>/g
+  var mention_regex = /<span class="tribute-mention">\((\S+\))<\/span>/g
   return markdown.replace(mention_regex, '[$1](mailto:$1)');
 };
 
@@ -10,7 +10,6 @@ export default class extends Controller {
   static targets = ['viewer', 'content']
 
   connect () {
-    console.log(this.contentTarget)
     const viewer = Editor.factory({
       el: this.viewerTarget,
       viewer: true,
