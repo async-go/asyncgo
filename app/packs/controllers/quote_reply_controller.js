b036738a6c54bf6874b251c67d6bcd75db011a8c
Jason Yavorska <jason@metaluna.io>
2021-05-05 09:12:21 +0200

    Standard fix

--- app/packs/controllers/quote_reply_controller.js
+++ app/packs/controllers/quote_reply_controller.js
@@ -1,16 +1,16 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ "content", "author", "date" ]
+  static targets = ['content', 'author', 'date']
 
   quote () {
     const content = this.contentTarget.innerText
     const date = this.dateTarget.innerText
     const author = this.authorTarget.innerText
     const commentbox = document.getElementById('comment_body')
-    var lines = content.split('\n')
+    const lines = content.split('\n')
     commentbox.value += 'On ' + date + ' ' + author + ' wrote:\n'
-    lines.forEach(element => commentbox.value += '> ' + element + '\n');
+    lines.forEach(element => commentbox.value += '> ' + element + '\n')
     commentbox.value += '\n'
     document.getElementById('comment_body').focus()
   }
