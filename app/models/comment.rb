fe5d9919ad061ace3313d8f65b6cb67c79d7761c
Matija Čupić <matteeyah@gmail.com>
2021-05-10 11:35:43 +0200

    Validate against image data in comment body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  IMAGEDATA_REGEX = %r{\(data:image/\w+;base64,[^\s)]+\)}i
+
   validates :body, presence: { allow_blank: false }
   validates :body_html, presence: true
+  validate :body_imagedata
 
   belongs_to :user
   belongs_to :topic
 
   has_many :notifications, as: :target, dependent: :destroy
   has_many :votes, as: :votable, dependent: :destroy
+
+  private
+
+  def body_imagedata
+    errors.add(:body, "can't contain base64 image data") if IMAGEDATA_REGEX.match?(body)
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,34 @@ RSpec.describe Comment, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:body) }
     it { is_expected.to validate_presence_of(:body_html) }
+
+    describe '#body_imagedata' do
+      subject(:valid?) { comment.valid? }
+
+      let(:comment) { FactoryBot.build(:comment) }
+
+      context 'when body does not have image data' do
+        before do
+          comment.body = 'hello world'
+        end
+
+        it { is_expected.to eq(true) }
+      end
+
+      context 'when body has image data' do
+        before do
+          comment.body = '![image.png](data:image/png;base64,abcdefg)'
+        end
+
+        it { is_expected.to eq(false) }
+
+        it 'adds an image data error to body' do
+          comment.valid?
+
+          expect(comment.errors.first).to have_attributes(attribute: :body)
+        end
+      end
+    end
   end
 
   describe 'Relations' do
