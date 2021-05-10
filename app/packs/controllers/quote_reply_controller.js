006ef1f725ffb7bf3348f8d63623f745b69d7a96
Jason Y <jason@asyncgo.com>
2021-05-10 16:55:17 +0200

    Use mentions in replies (#540)
    * Use mentions in replies
    
    * Fix interpolation

--- app/packs/controllers/quote_reply_controller.js
+++ app/packs/controllers/quote_reply_controller.js
@@ -1,15 +1,15 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['content', 'author', 'date']
+  static targets = ['content', 'authoremail', 'date']
 
   quote () {
     const content = this.contentTarget.innerText
     const date = this.dateTarget.innerText
-    const author = this.authorTarget.innerText
+    const authorEmail = this.authoremailTarget.innerText.trim()
 
     const quotedReply = content.split('\n').map(line => `> ${line}`).join('\n')
-    document.getElementById('comment_body').value = `On ${date} ${author} wrote\n${quotedReply}\n\n`
+    document.getElementById('comment_body').value = `On ${date} @${authorEmail} wrote:\n${quotedReply}\n\n`
 
     document.getElementById('comment_body').focus()
   }
