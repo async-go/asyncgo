ddf8cc6328249b51d432506b1c5b09da5061a275
Jason Yavorska <jason@metaluna.io>
2021-05-11 08:40:40 +0200

    Hot patch revert of issue causing failed pipelines

---
+++ config/initializers/webpack_patch.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# config/initializers/webpacker_patch.rb
+# revert of https://github.com/rails/webpacker/commit/3760588534c54527d21c684c2cb5ca30cafc0901
+module WebpackerChdirPatch
+  def watched_files_digest
+    unless watched_paths.empty?
+      warn 'Webpacker::Compiler.watched_paths has been deprecated. Set additional_paths in webpacker.yml instead.'
+    end
+
+    files = Dir[*default_watched_paths, *watched_paths].reject { |f| File.directory?(f) }
+    file_ids = files.sort.map { |f| "#{File.basename(f)}/#{Digest::SHA1.file(f).hexdigest}" }
+    Digest::SHA1.hexdigest(file_ids.join('/'))
+  end
+end
+
+Webpacker::Compiler.prepend WebpackerChdirPatch
