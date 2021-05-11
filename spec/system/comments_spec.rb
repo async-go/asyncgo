dca7744fdc9f0253d4101b1439b0494da57b69d3
Jason Yavorska <jason@metaluna.io>
2021-05-11 08:49:34 +0200

    Rubocop

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -52,8 +52,8 @@ RSpec.describe 'Comments', type: :system do
     click_link 'Topics'
     click_link topic.title
 
-    expect(page).to have_selector("#editor_comment_new")
-    tuieditor_setcontent("editor_comment_new", '![image.png](data:image/png;base64,abcdefg)')
+    expect(page).to have_selector('#editor_comment_new')
+    tuieditor_setcontent('editor_comment_new', '![image.png](data:image/png;base64,abcdefg)')
     click_button 'Add Comment'
 
     expect(page).to have_text("Body can't contain embedded markdown images")
