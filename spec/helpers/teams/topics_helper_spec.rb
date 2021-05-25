8bd7fc0f7ab5f23762455485426ed1a47c017df1
Jason Yavorska <jason@metaluna.io>
2021-05-25 08:08:49 +0200

    Fix test

--- spec/helpers/teams/topics_helper_spec.rb
+++ spec/helpers/teams/topics_helper_spec.rb
@@ -54,7 +54,7 @@ RSpec.describe Teams::TopicsHelper, type: :helper do
         end
 
         it { is_expected.to have_text('Due 1 day ago') }
-        it { is_expected.to match(/class="theme-light"/) }
+        it { is_expected.to match(/class="text-dark bg-warning"/) }
       end
 
       context 'when topic is not overdue' do
