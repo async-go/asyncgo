4ca831be89991a22e597d632bbe45614d91a8d4e
Matija Čupić <matteeyah@gmail.com>
2021-07-03 16:21:33 +0200

    Add staging environment

--- spec/mailers/previews/digest_mailer_preview.rb
+++ spec/mailers/previews/digest_mailer_preview.rb
@@ -5,27 +5,31 @@ require 'factory_bot'
 # Preview all emails at http://localhost:3000/rails/mailers/digest_mailer
 class DigestMailerPreview < ActionMailer::Preview
   def digest_email
-    team = Team.create!(id: 1001, name: 'sample team')
+    seed_data
 
-    user = User.new(id: 1001, email: 'digestmailer-user@preview.com', name: 'Bob Test', team: team)
-    user.preferences = User::Preferences.new(user: user)
-    user.save!
-
-    actor = User.new(id: 2001, email: 'digestmailer-actor@preview.com', name: 'Actor Sample', team: team)
-    actor.preferences = User::Preferences.new(user: actor)
-    actor.save!
-
-    topic = Topic.create!(id: 1001, title: 'Test Topic', team: team, user: user, status: :resolved,
-                         description: 'Hello', description_html: '<p>Hello</p>')
-
-    Notification.create!(id: 1001, user: user, actor: actor, target: topic, action: 'created')
-    Topic.create!(id: 2001, title: 'Due Topic', team: team, user: user, status: :active, due_date: Date.today,
-                  description: 'Hello', description_html: '<p>Hello</p>')
-
-    DigestMailer.with(user: user).digest_email.message.tap do
+    DigestMailer.with(user: User.find(1001)).digest_email.message.tap do
       Team.find(1001).destroy
       User.find(1001).destroy
       User.find(2001).destroy
     end
+  end
+
+  private
+
+  def seed_data
+    team = Team.create!(id: 1001, name: 'sample team')
+
+    user = User.new(id: 1001, email: 'digestmailer-user@preview.com', name: 'Bob Test', team: team)
+    user.update!(preferences: User::Preferences.new(user: user))
+
+    actor = User.new(id: 2001, email: 'digestmailer-actor@preview.com', name: 'Actor Sample', team: team)
+    actor.update!(preferences: User::Preferences.new(user: actor))
+
+    topic = Topic.create!(id: 1001, title: 'Test Topic', team: team, user: user, status: :resolved,
+                          description: 'Hello', description_html: '<p>Hello</p>')
+
+    Notification.create!(id: 1001, user: user, actor: actor, target: topic, action: 'created')
+    Topic.create!(id: 2001, title: 'Due Topic', team: team, user: user, status: :active, due_date: Time.zone.today,
+                  description: 'Hello', description_html: '<p>Hello</p>')
   end
 end
