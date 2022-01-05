e4a6ffa19ab3248e8f730451878e2be1636e126e
Jason Yavorska <jason@metaluna.io>
2022-01-05 14:57:50 +0100

    Rubocop fixes

--- spec/finders/topics_finder_spec.rb
+++ spec/finders/topics_finder_spec.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe TopicsFinder, type: :finder do
-  let(:team) { FactoryBot.create(:team) }
+  let(:team) { create(:team) }
   let(:finder) { described_class.new(team, [], {}) }
 
   describe '#call' do
     subject(:call) { finder.call }
 
     it 'includes specified relations' do
-      fail
+      raise 'not implemented: includes specified relations'
     end
 
     it 'filters topics by status' do
-      fail
+      raise 'not implemented: filters topics by status'
     end
 
     it 'orders topics by pinned' do
-      fail
+      raise 'not implemented: orders topics by pinned'
     end
 
     it 'orders topics by sort' do
+      raise 'not implemented: orders topics by sort'
     end
   end
 end
