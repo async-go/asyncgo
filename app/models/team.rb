3ba193e2fcd6243c3b26e2de82bdf7e65189ba95
Jason Yavorska <jason@metaluna.io>
2022-03-24 09:05:20 +0100

    Make it more clear what characters are invalid

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
-  validates :name, presence: { allow_blank: false, allow_empty: false }, format: { without: %r{[:\\/]} }
+  INVALID_CHARS = ':\/'
+
+  validates :name, presence: { allow_blank: false, allow_empty: false },
+                   format: { without: Regexp.new("[#{Regexp.escape(INVALID_CHARS)}]") }
   validates :message, presence: { allow_blank: false, allow_empty: false, allow_nil: true }
 
   has_one :subscription, class_name: 'Team::Subscription', dependent: :destroy
